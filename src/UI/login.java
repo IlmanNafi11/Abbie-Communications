0ee64389cd762f97c8d40a30c4f59fafff70a6ab
IlmanNafi11 <studyilmannafi@gmail.com>
2024-02-24 07:51:16 +0700

    add button icon login page

--- src/UI/login.java
+++ src/UI/login.java
@@ -30,15 +30,30 @@ public class login extends javax.swing.JInternalFrame {
 
         jPanel1.setLayout(null);
 
+        btnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-Login-Primary-Default.png"))); // NOI18N
         btnLogin.setBorder(null);
         btnLogin.setContentAreaFilled(false);
+        btnLogin.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnLoginMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnLoginMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnLoginMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btnLoginMousePressed(evt);
+            }
+        });
         btnLogin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnLoginActionPerformed(evt);
             }
         });
         jPanel1.add(btnLogin);
-        btnLogin.setBounds(196, 530, 364, 52);
+        btnLogin.setBounds(196, 531, 364, 52);
 
         txtUsername.setBackground(new java.awt.Color(242, 242, 242));
         txtUsername.setBorder(null);
@@ -50,8 +65,20 @@ public class login extends javax.swing.JInternalFrame {
         jPanel1.add(txtPassword);
         txtPassword.setBounds(250, 458, 300, 44);
 
+        btnRfid.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-RFID-Primary-Default.png"))); // NOI18N
         btnRfid.setBorder(null);
         btnRfid.setContentAreaFilled(false);
+        btnRfid.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnRfidMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnRfidMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btnRfidMousePressed(evt);
+            }
+        });
         btnRfid.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRfidActionPerformed(evt);
@@ -60,29 +87,53 @@ public class login extends javax.swing.JInternalFrame {
         jPanel1.add(btnRfid);
         btnRfid.setBounds(196, 692, 364, 52);
 
+        lblBack.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Back-Default.png"))); // NOI18N
         lblBack.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 lblBackMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblBackMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblBackMouseExited(evt);
+            }
         });
         jPanel1.add(lblBack);
-        lblBack.setBounds(78, 172, 60, 25);
+        lblBack.setBounds(73, 165, 60, 40);
 
+        lblRegister.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Register-Default.png"))); // NOI18N
         lblRegister.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 lblRegisterMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblRegisterMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblRegisterMouseExited(evt);
+            }
         });
         jPanel1.add(lblRegister);
-        lblRegister.setBounds(435, 773, 115, 20);
+        lblRegister.setBounds(438, 775, 115, 20);
 
+        lblForgot.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Forgot-Default.png"))); // NOI18N
         lblForgot.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 lblForgotMouseClicked(evt);
             }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblForgotMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblForgotMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                lblForgotMousePressed(evt);
+            }
         });
         jPanel1.add(lblForgot);
-        lblForgot.setBounds(418, 600, 144, 20);
+        lblForgot.setBounds(420, 601, 144, 20);
 
         bg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Login.png"))); // NOI18N
         jPanel1.add(bg);
@@ -111,6 +162,7 @@ public class login extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_btnLoginActionPerformed
 
     private void lblForgotMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblForgotMouseClicked
+        lblForgot.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Forgot-Hover.png")));
         forgotPassword_1 rg = new forgotPassword_1();
         this.getDesktopPane().add(rg);
         rg.setVisible(true);
@@ -127,6 +179,62 @@ public class login extends javax.swing.JInternalFrame {
         rg.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_lblRegisterMouseClicked
+
+    private void lblBackMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBackMouseEntered
+        lblBack.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Back-Hover.png")));
+    }//GEN-LAST:event_lblBackMouseEntered
+
+    private void lblBackMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBackMouseExited
+        lblBack.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Back-Default.png")));
+    }//GEN-LAST:event_lblBackMouseExited
+
+    private void btnLoginMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnLoginMouseClicked
+        btnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-Login-Primary-Click.png")));
+    }//GEN-LAST:event_btnLoginMouseClicked
+
+    private void btnLoginMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnLoginMouseEntered
+        btnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-Login-Primary-Hover.png")));
+    }//GEN-LAST:event_btnLoginMouseEntered
+
+    private void btnLoginMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnLoginMouseExited
+        btnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-Login-Primary-Default.png")));
+    }//GEN-LAST:event_btnLoginMouseExited
+
+    private void btnLoginMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnLoginMousePressed
+        btnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-Login-Primary-Click.png")));
+    }//GEN-LAST:event_btnLoginMousePressed
+
+    private void lblForgotMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblForgotMouseEntered
+        lblForgot.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Forgot-Hover.png")));
+    }//GEN-LAST:event_lblForgotMouseEntered
+
+    private void lblForgotMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblForgotMouseExited
+        lblForgot.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Forgot-Default.png")));
+    }//GEN-LAST:event_lblForgotMouseExited
+
+    private void lblForgotMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblForgotMousePressed
+        lblForgot.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Forgot-Hover.png")));
+    }//GEN-LAST:event_lblForgotMousePressed
+
+    private void btnRfidMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRfidMouseEntered
+        btnRfid.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-RFID-Hover.png")));
+    }//GEN-LAST:event_btnRfidMouseEntered
+
+    private void btnRfidMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRfidMouseExited
+        btnRfid.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-RFID-Primary-Default.png")));
+    }//GEN-LAST:event_btnRfidMouseExited
+
+    private void btnRfidMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRfidMousePressed
+        btnRfid.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Btn-RFID-Click.png")));
+    }//GEN-LAST:event_btnRfidMousePressed
+
+    private void lblRegisterMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRegisterMouseEntered
+        lblRegister.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Register-Hover.png")));
+    }//GEN-LAST:event_lblRegisterMouseEntered
+
+    private void lblRegisterMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRegisterMouseExited
+        lblRegister.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ButtonIcon/Label-Register-Default.png")));
+    }//GEN-LAST:event_lblRegisterMouseExited
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
