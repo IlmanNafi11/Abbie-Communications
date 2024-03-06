ba610f7a89540a119c0da507d20d62dd2a6f5bc4
IlmanNafi11 <studyilmannafi@gmail.com>
2024-03-07 00:06:29 +0700

    koding kelas koneksi database

--- src/Connections/ClassKoneksi.java
+++ src/Connections/ClassKoneksi.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Connections;
 
-/**
- *
- * @author LENOVO
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class ClassKoneksi {
-    
+
+    public static Connection pengkoneksi;
+
+    public static Connection GetConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            pengkoneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/abbie_communication", "root", "");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Koneksi Gagal !!");
+        }
+        return pengkoneksi;
+    }
 }
