b4b0e1825de3fd0a80ad2bc42bf4b9ea2a8d53cb
IlmanNafi11 <studyilmannafi@gmail.com>
2024-03-16 19:41:19 +0700

    koding logika tambah, ubah, dan delete member

---
+++ src/Data_Acces/DbMember.java
@@ -0,0 +1,132 @@
+package Data_Acces;
+
+import Connections.ClassKoneksi;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import Logic.*;
+
+public class DbMember {
+
+    private String idMember;
+    private String noHp;
+    private String namaMember;
+    private String alamatMember;
+    private ExceptionHandler exceptionHandler;
+
+    public DbMember(String idMember, String noHp, String namaMember, String alamatMember) {
+        this.idMember = idMember;
+        this.noHp = noHp;
+        this.namaMember = namaMember;
+        this.alamatMember = alamatMember;
+        exceptionHandler = new ExceptionHandler();
+    }
+
+    public DbMember(String noHp) {
+        this.noHp = noHp;
+        exceptionHandler = new ExceptionHandler();
+    }
+
+    public boolean cekMember() {
+        String queryCek = "SELECT COUNT(*) FROM member WHERE tlp_member = ?";
+        try {
+            Connection koneksi = ClassKoneksi.GetConnection();
+            PreparedStatement stCekMember = koneksi.prepareStatement(queryCek);
+            stCekMember.setString(1, noHp);
+            ResultSet rs = stCekMember.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+        } catch (Exception e) {
+            exceptionHandler.getErrorKesalahan("Failed to check member data " + e);
+        }
+        return false;
+    }
+
+    public boolean InsertMember() {
+        String queryInsert = "INSERT INTO member (id_member, nama_member, tlp_member, alamat_member) VALUES (?, ?, ?, ?)";
+        boolean confirm = exceptionHandler.confirmSaveDataPerson("Save Member Data?");
+        try {
+            Connection koneksi = ClassKoneksi.GetConnection();
+            PreparedStatement stInsert = koneksi.prepareStatement(queryInsert);
+            stInsert.setString(1, idMember);
+            stInsert.setString(2, namaMember);
+            stInsert.setString(3, noHp);
+            stInsert.setString(4, alamatMember);
+            if (confirm) {
+                stInsert.executeUpdate();
+                exceptionHandler.succesSavePersonData("Member data has been successfully saved");
+                return true;
+            }
+        } catch (Exception e) {
+            exceptionHandler.getErrorKesalahan("Failed when trying to save member data " + e);
+        }
+        return false;
+    }
+
+    public boolean ChangeMember() {
+        String queryUpdate = "UPDATE member SET nama_member = ?, tlp_member = ?, alamat_member = ? WHERE id_member = ?";
+        boolean confirm = exceptionHandler.confirmChangePerson("Change Member Data?");
+        try {
+            Connection koneksi = ClassKoneksi.GetConnection();
+            PreparedStatement stChange = koneksi.prepareStatement(queryUpdate);
+            stChange.setString(1, namaMember);
+            stChange.setString(2, noHp);
+            stChange.setString(3, alamatMember);
+            stChange.setString(4, idMember);
+            if (confirm) {
+                stChange.executeUpdate();
+                exceptionHandler.succesSavePersonData("Member data has been successfully Changed");
+                return true;
+            }
+        } catch (Exception e) {
+            exceptionHandler.getErrorKesalahan("Failed when trying to change member data " + e);
+        }
+        return false;
+    }
+
+    public boolean DeleteMember() {
+        String queryDelete = "DELETE FROM member WHERE id_member = ?";
+        boolean confirm = exceptionHandler.confirmDeleteData("Are you sure you deleted member data?");
+        try {
+            Connection koneksi = ClassKoneksi.GetConnection();
+            PreparedStatement stDelete = koneksi.prepareStatement(queryDelete);
+            stDelete.setString(1, idMember);
+            if (confirm) {
+                stDelete.executeUpdate();
+                exceptionHandler.succesDeleteData("Member data has been successfully deleted");
+                return true;
+            }
+        } catch (Exception e) {
+            exceptionHandler.getErrorKesalahan("Failed when trying to delete member data " + e);
+        }
+        return false;
+    }
+
+    // get data member utk insert ke table
+    public ConfigTable GetAllDataMember() {
+        ConfigTable dataTable = new ConfigTable();
+        String queryGetData = "SELECT * FROM member";
+        dataTable.addColumn("Member ID");
+        dataTable.addColumn("Member Name");
+        dataTable.addColumn("Telephone");
+        dataTable.addColumn("Address");
+        try {
+            Connection koneksi = ClassKoneksi.GetConnection();
+            PreparedStatement stGetData = koneksi.prepareStatement(queryGetData);
+            ResultSet rs = stGetData.executeQuery();
+            while (rs.next()) {
+                dataTable.addRow(new Object[]{
+                    rs.getString("id_member"),
+                    rs.getString("nama_member"),
+                    rs.getString("tlp_member"),
+                    rs.getString("alamat_member"),});
+            }
+        } catch (Exception e) {
+            exceptionHandler.getErrorKesalahan("Failed when trying to get data " + e);
+        }
+        return dataTable;
+    }
+
+}
---
+++ src/Logic/MemberControler.java
@@ -0,0 +1,143 @@
+package Logic;
+
+import Data_Acces.DbMember;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JTable;
+
+public class MemberControler {
+
+    private String idMember;
+    private String noHp;
+    private String namaMember;
+    private String alamatMember;
+    private ExceptionHandler exceptionHandler;
+
+    public MemberControler(String idMember, String noHp, String namaMember, String alamatMember) {
+        this.idMember = idMember;
+        this.noHp = noHp;
+        this.namaMember = namaMember;
+        this.alamatMember = alamatMember;
+        exceptionHandler = new ExceptionHandler();
+    }
+
+    private String GenerateRandom(int angka) {
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < angka; i++) {
+            stringBuilder.append(random.nextInt(10));
+        }
+        return stringBuilder.toString();
+    }
+
+    private String GenerateIdMember() {
+        return idMember = "MBR" + GenerateRandom(4);
+    }
+
+    private boolean ValidateName() {
+        if (namaMember.matches("[a-zA-Z .]+")) {
+            if (namaMember.length() <= 25) {
+                return true;
+            } else {
+                exceptionHandler.getErrorKesalahan("Member names must not exceed 25 characters !");
+            }
+        } else {
+            exceptionHandler.getErrorKesalahan("Invalid member name !");
+        }
+        return false;
+    }
+
+    private boolean cekNoHp() {
+        DbMember dbMember = new DbMember(noHp);
+        boolean cek = dbMember.cekMember();
+        if (!cek) {
+            return true;
+        } else {
+            exceptionHandler.getErrorKesalahan("Telephone number has been used !");
+        }
+        return false;
+    }
+
+    private boolean ValidateNoHp() {
+        DbMember dbMember = new DbMember(noHp);
+        if (noHp.matches("\\d+") && noHp.length() > 11 && noHp.length() < 14) {
+            boolean cekNoHp = dbMember.cekMember();
+            return true;
+        } else {
+            exceptionHandler.getErrorKesalahan("Invalid cellphone number !");
+        }
+        return false;
+    }
+
+    private boolean ValidateAddress() {
+        if (alamatMember.length() <= 50) {
+            return true;
+        } else {
+            exceptionHandler.getErrorKesalahan("The address must not exceed 50 characters !");
+        }
+        return false;
+    }
+
+    private boolean ValidateField() {
+        if (!namaMember.equalsIgnoreCase("Member Name") && !namaMember.equals("") && !noHp.equalsIgnoreCase("Telephone Number") && !noHp.equals("")
+                && !alamatMember.equalsIgnoreCase("Address") && !namaMember.equals("")) {
+            if (ValidateName() && ValidateNoHp() && ValidateAddress()) {
+                return true;
+            }
+        } else {
+            exceptionHandler.getErrorKesalahan("The field cannot be empty !");
+        }
+        return false;
+    }
+
+    public boolean InsertMember() {
+        GenerateIdMember();
+        if (ValidateField() && cekNoHp()) {
+            DbMember dbMember = new DbMember(idMember, noHp, namaMember, alamatMember);
+            boolean succes = dbMember.InsertMember();
+            if (succes) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean ChangeMember() {
+        if (ValidateField()) {
+            DbMember dbMember = new DbMember(idMember, noHp, namaMember, alamatMember);
+            boolean succes = dbMember.ChangeMember();
+            if (succes) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void DeleteMember(int row, JTable table) {
+        if (row != -1) {
+            this.idMember = table.getValueAt(row, 0).toString();
+            DbMember dbMember = new DbMember(idMember, noHp, namaMember, alamatMember);
+            dbMember.DeleteMember();
+        } else {
+            exceptionHandler.getErrorKesalahan("Select the data you want to delete!");
+        }
+    }
+
+    public ConfigTable GetAllData() {
+        DbMember dbMember = new DbMember(idMember, noHp, namaMember, alamatMember);
+        return dbMember.GetAllDataMember();
+    }
+
+    public ArrayList<String> IsiField(int row, JTable table) {
+        ArrayList<String> data = new ArrayList<>();
+        String memberId = table.getValueAt(row, 0).toString();
+        String nama = table.getValueAt(row, 1).toString();
+        String noHpMember = table.getValueAt(row, 2).toString();
+        String alamat = table.getValueAt(row, 3).toString();
+        data.add(memberId);
+        data.add(nama);
+        data.add(noHpMember);
+        data.add(alamat);
+        return data;
+    }
+}
